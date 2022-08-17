cd09e59f205b1f1984481d678c31ef66533d1473
debysouza <leticiavieirach@gmail.com>
2022-08-17 13:15:50 -0300

    Listar por ativos

--- src/main/java/br/com/neki/desafio/repositories/SkillRepository.java
+++ src/main/java/br/com/neki/desafio/repositories/SkillRepository.java
@@ -1,11 +1,17 @@
 package br.com.neki.desafio.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.neki.desafio.entities.Skill;
 
 @Repository
 public interface SkillRepository extends JpaRepository<Skill, Integer> {
+	
+	@Query(value = "select * from teste_residencia.skill where ativo = true", nativeQuery = true)
+	List<Skill> findAllAtivos();
 
 }
--- src/main/java/br/com/neki/desafio/repositories/UserRepository.java
+++ src/main/java/br/com/neki/desafio/repositories/UserRepository.java
@@ -1,6 +1,9 @@
 package br.com.neki.desafio.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.neki.desafio.entities.User;
@@ -9,5 +12,8 @@ import br.com.neki.desafio.entities.User;
 public interface UserRepository extends JpaRepository<User, Integer> {
 	
 	User findByLoginUsuario(String login);
+	
+	@Query(value = "select * from teste_residencia.user where ativo = true", nativeQuery = true)
+	List<User> findAllAtivos();
 
 }
