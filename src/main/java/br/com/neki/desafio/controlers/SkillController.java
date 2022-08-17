7120029c600787cdacabb9206f2cf2e5b5cbad2a
debysouza <leticiavieirach@gmail.com>
2022-08-17 13:16:09 -0300

    Adição de endpoints

--- src/main/java/br/com/neki/desafio/controlers/SkillController.java
+++ src/main/java/br/com/neki/desafio/controlers/SkillController.java
@@ -2,15 +2,24 @@ package br.com.neki.desafio.controlers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.neki.desafio.entities.Skill;
+import br.com.neki.desafio.entities.User;
 import br.com.neki.desafio.services.SkillService;
 
 @RestController
@@ -25,5 +34,34 @@ public class SkillController {
 		HttpHeaders headers = new HttpHeaders();
 		return new ResponseEntity<>(habilidadeService.findAll(), headers, HttpStatus.OK);
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Skill> findById(@PathVariable Integer id) {
+		HttpHeaders headers = new HttpHeaders();
+		return new ResponseEntity<>(habilidadeService.findById(id), headers, HttpStatus.OK);
+	}
 
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Integer id) {
+		habilidadeService.deleteLogical(id);
+	}
+	
+	@PutMapping("/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Skill update(@Valid @RequestBody Skill skill, @PathVariable Integer id) {
+		return habilidadeService.update(skill, id);
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Skill> save(@Valid @RequestBody Skill skill) {
+		HttpHeaders headers = new HttpHeaders();
+		Skill newHabilidade = habilidadeService.save(skill);
+		if(null != newHabilidade) {
+			return new ResponseEntity<>(newHabilidade, headers, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(newHabilidade, headers, HttpStatus.BAD_REQUEST);
+		}
+	
+	}
 }
--- src/main/java/br/com/neki/desafio/controlers/UserController.java
+++ src/main/java/br/com/neki/desafio/controlers/UserController.java
@@ -8,11 +8,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.neki.desafio.dto.UserDTO;
 import br.com.neki.desafio.entities.User;
 import br.com.neki.desafio.services.UserService;
 
@@ -32,5 +39,29 @@ public class UserController {
 	@GetMapping("/login")
 	public String loginApp(@Valid @RequestParam String login, @RequestParam String senha) {
 		return usuarioService.loginApp(login, senha);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<User> findById(@PathVariable Integer id) {
+		HttpHeaders headers = new HttpHeaders();
+		return new ResponseEntity<>(usuarioService.findById(id), headers, HttpStatus.OK);
+	}
+
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Integer id) {
+		usuarioService.deleteLogical(id);
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<User> save(@Valid @RequestBody UserDTO usuario) {
+		HttpHeaders headers = new HttpHeaders();
+		return new ResponseEntity<>(usuarioService.save(usuario), headers, HttpStatus.OK);
+	}
+	
+	@PutMapping("/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public User update(@Valid @RequestBody User user, @PathVariable Integer id) {
+		return usuarioService.update(user, id);
 	}
 }
