9659c74933ba0fc42b33cac4bc67f7952d0c48a8
debysouza <leticiavieirach@gmail.com>
2022-08-17 13:14:54 -0300

    Adição de métodos

--- src/main/java/br/com/neki/desafio/services/SkillService.java
+++ src/main/java/br/com/neki/desafio/services/SkillService.java
@@ -1,6 +1,7 @@
 package br.com.neki.desafio.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,7 +16,31 @@ public class SkillService {
 	private SkillRepository habilidadeRepository;
 	
 	public List<Skill> findAll() {
-		return habilidadeRepository.findAll();
+		return habilidadeRepository.findAllAtivos();
 	}
+	
+	public Skill findById(Integer id) {
+		return habilidadeRepository.findById(id).get();
+	}
+	
+	public void deleteLogical(Integer id) {
+		Optional<Skill> habilidade = habilidadeRepository.findById(id);
+		if(habilidade.isPresent()) {
+			Skill usu = habilidade.get();
+			usu.setAtivo(false);
+			habilidadeRepository.save(usu);
+		}
+	}
+	
+	public Skill save(Skill habilidade) {
+		Skill newHabilidade = habilidadeRepository.save(habilidade);
+		return newHabilidade;
+	}
+	
+	public Skill update(Skill habilidade, Integer id) {
+		habilidade.setId(id);
+		return habilidadeRepository.save(habilidade);
+	}
+
 
 }
--- src/main/java/br/com/neki/desafio/services/UserService.java
+++ src/main/java/br/com/neki/desafio/services/UserService.java
@@ -1,11 +1,14 @@
 package br.com.neki.desafio.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.neki.desafio.dto.UserDTO;
 import br.com.neki.desafio.entities.User;
+import br.com.neki.desafio.mapper.EntidadeDTOMapper;
 import br.com.neki.desafio.repositories.UserRepository;
 
 @Service
@@ -15,7 +18,11 @@ public class UserService {
 	private UserRepository usuarioRepository;
 
 	public List<User> findAll() {
-		return usuarioRepository.findAll();
+		return usuarioRepository.findAllAtivos();
+	}
+	
+	public User findById(Integer id) {
+		return usuarioRepository.findById(id).get();
 	}
 
 	public String loginApp(String login, String senha) {
@@ -31,6 +38,27 @@ public class UserService {
 		} else {
 			return "Login e/ou senha inválidos.";
 		}
+	}
+	
+	public void deleteLogical(Integer id) {
+		Optional<User> usuario = usuarioRepository.findById(id);
+		if(usuario.isPresent()) {
+			User usu = usuario.get();
+			usu.setAtivo(false);
+			usuarioRepository.save(usu);
+		}
+	}
+	
+	public User save(UserDTO usuarioDTO) {
+		EntidadeDTOMapper entidadeDTOMapper = new EntidadeDTOMapper();
+		User newUsuario = new User();
+		entidadeDTOMapper.userDTOToUser(newUsuario, usuarioDTO);
+		return usuarioRepository.save(newUsuario);
+	}
+	
+	public User update(User usuario, Integer id) {
+		usuario.setId(id);
+		return usuarioRepository.save(usuario);
 	}
 
 }
