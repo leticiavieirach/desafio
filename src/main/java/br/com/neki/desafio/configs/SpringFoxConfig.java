c22e10e5838f407c16dc04b4546c7b1c20af1f60
debysouza <leticiavieirach@gmail.com>
2022-08-18 08:13:33 -0300

    Ajuste na configuração do swagger

--- src/main/java/br/com/neki/desafio/configs/SpringFoxConfig.java
+++ src/main/java/br/com/neki/desafio/configs/SpringFoxConfig.java
@@ -2,7 +2,6 @@ package br.com.neki.desafio.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -12,7 +11,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SpringFoxConfig extends WebMvcConfigurerAdapter {
+public class SpringFoxConfig {
 
     @Bean
     Docket api() {
